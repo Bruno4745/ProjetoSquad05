d30a2fa32053d9154c2eecd78433182e24d3c8f1
EdmarMiller <edmarmiller@hotmail.com>
2021-01-18 14:08:26 -0300

    Card da pagina de Parceiro

--- src/Pages/ProcurarAjuda/index.jsx
+++ src/Pages/ProcurarAjuda/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Card from '../../Components/Card/Card';
 const ProcurarAjuda = () => {
     return(
         <div>
-            ProcurarAjuda
+            <Card
+            title ='Card Title'
+            imageUrl ='https://s3.amazonaws.com/kp-blog/wp-content/uploads/2019/07/12110920/tamanho-de-imagem-instagram-formato-square.png'
+            body = 'Conteudo Textual do Card' />
         </div>
     )
 };
