702c6df428d4df276897b8a66933cc5694f25abc
EdmarMiller <edmarmiller@hotmail.com>
2021-01-18 22:28:55 -0300

    Ajuste na Barra de navegação

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -14,16 +14,19 @@ const Menu = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse>
                         <Nav className="mr-auto">
+                        <Nav.Link as={Link} href="/sobrenos" to="/sobrenos">Sobre Nós</Nav.Link>
                         <NavDropdown title="Informativo">
-                            <NavDropdown.Item as={Link} href="/gestanteMae" to="/gestanteMae">Gestante/Mae</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} href="/gestanteMae" to="/gestanteMae">Gestante/Mãe</NavDropdown.Item>
                             <NavDropdown.Item as={Link} href="/profissional" to="/profissional">Profissional</NavDropdown.Item>
                         </NavDropdown>
                             {/* <Nav.Link as={Link} href="/informativo" to="/informativo">Informativo</Nav.Link> */}
                             <Nav.Link as={Link} href="/procurarAjuda" to="/procurarAjuda">Onde Procurar Ajuda</Nav.Link>
-                            <Nav.Link as={Link} href="/depoimentos" to="/depoimentos">Depoimentos</Nav.Link>
-                            <Nav.Link as={Link} href="/faleConosco" to="/faleConosco">Fale Conosco</Nav.Link>
-                            <Nav.Link as={Link} href="/denuncia" to="/denuncia">Denuncia</Nav.Link>
-                            <Nav.Link as={Link} href="/sobrenos" to="/sobrenos">Sobre Nós</Nav.Link>
+                        <NavDropdown title="Fale Conosco">
+                            <NavDropdown.Item as={Link} href="/contatos" to="/contatos">Contatos</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} href="/denuncia" to="/denuncia">Denúncia</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} href="/depoimentos" to="/depoimentos">Depoimentos</NavDropdown.Item>
+                        </NavDropdown>
+                            
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import Denuncia from './Pages/Denuncia'
 import Depoimentos from './Pages/Depoimentos'
-import faleConosco from './Pages/FaleConosco'
+import Contatos from './Pages/Contatos'
 import Home from './Pages/Home'
 import GestanteMae from './Pages/InfoGestante/'
 import InfoProfissional from './Pages/InfoProfissional'
@@ -17,7 +17,7 @@ const Routes = () => {
             <Route exact path='/profissional' component={InfoProfissional} />
             <Route exact path='/procurarAjuda' component={ProcurarAjuda} />
             <Route exact path='/depoimentos' component={Depoimentos} />
-            <Route exact path='/faleConosco' component={faleConosco} />
+            <Route exact path='/contatos' component={Contatos} />
             <Route exact path='/denuncia' component={Denuncia} />
             <Route exact path='/sobrenos' component={SobreNos} />
 
