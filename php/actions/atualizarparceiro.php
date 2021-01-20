d0152b211d0afb4887bb337eeee9128b2c9100c5
Bruno4745 <bruno_4745@hotmail.com>
2021-01-19 23:42:46 -0300

    correcao para atualizar parceiros

--- php/actions/atualizarparceiro.php
+++ php/actions/atualizarparceiro.php
@@ -6,13 +6,22 @@ require_once "conexao.php";
 
     if($botao == 'Alterar'){
         $id = $_GET['id'];
+        $categoria = $_POST['categoria'];
         $nome = $_POST['nome'];
+        $logradouro = $_POST['logradouro'];
+        $numero = $_POST['numero'];
+        $complemento = $_POST['complemento'];
         $cidade = $_POST['cidade'];
+        $estado = $_POST['estado'];
+        $cep = $_POST['cep'];
+        $contato = $_POST['contato'];
         $telefone = $_POST['telefone'];
+        $email = $_POST['email'];
+        $site = $_POST['site'];
 
         echo $id, $nome, $cidade, $telefone;
 
-        $sql = "UPDATE parceiros SET nome='$nome', cidade='$cidade', telefone='$telefone' WHERE id_parceiros = $id;";
+        $sql = "UPDATE parceiros SET categoria='$categoria', nome='$nome', logradouro='$logradouro', numero='$numero', complemento='$complemento', cidade='$cidade', estado='$estado', cep='$cep', contato='$contato', telefone='$telefone', email='$email', site='$site' WHERE id_parceiros = $id;";
         nonquery($sql);
     }
         header("Location: http://localhost:3000/procurarAjudaAdm");
