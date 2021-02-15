7e1977b77dc8cff00a3d242610bf1936f2133b44
Bruno4745 <bruno_4745@hotmail.com>
2021-02-15 00:14:22 -0300

    Incluido criptografia da pagina denuncia

--- backend/routes/denunciaRoute.js
+++ backend/routes/denunciaRoute.js
@@ -1,18 +1,28 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (app) => {
     
     app.post('/faleconosco/denuncia', (req, res) => {
 
         const conteudo = req.body;
-        console.log(conteudo);
+        // console.log(conteudo);
+        var sal = bcrypt.genSaltSync(10); 
+        var hash = bcrypt.hashSync(conteudo.mensagem, sal);
+        conteudo.mensagem = hash;
+        var hash = bcrypt.hashSync(conteudo.horario, sal);
+        conteudo.horario = hash;
+        // console.log(conteudo);
+        
         var connection = app.config.database();
         var query = app.models.denunciaModel;
-
+        
         query.postDenuncia(conteudo, connection, (err, result) => {
             if(!err) {
             res.json(result);
             } else {
             console.log(err);
             };
+            console.log(conteudo);
         });
     });
 };
